c5b68b61f14e4367768452d91a29ec688452bf88
Edwin Gutierrez <84048027+edguti3rrez@users.noreply.github.com>
2021-06-04 17:22:41 -0700

    Add files via upload

---
+++ p2-random.js
@@ -0,0 +1,31 @@
+/*
+    CIT 281 Project 2
+    Name: Edwin Gutierrez
+*/
+
+// Returns a random number between min (inclusive) and max (exclusive)
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+let result = "";
+
+// Will return a random length string between minLength and maxLength
+function getRandomString(minLength, maxLength) {
+
+    for (let i = 0; i < getRandomInteger(minLength, maxLength); i++) {
+        result += getRandomLetter();
+    }
+    return getSortedString(result);
+}
+
+// Will return a single random undercase letter from the alphabet array
+function getRandomLetter() {
+    return alphabet[getRandomInteger(0,alphabet.length-1)]
+}
+
+// will sort and return a string in ascending order
+function getSortedString(string) { return string.split("").sort().join(""); }
+
+console.log(getRandomString(10, 20));
